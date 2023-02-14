40427ecf58f9aca2bdd6109e379c31e619cc9682
levy <lnjiru43@gmail.com>
2023-02-14 06:07:28 +0300

    completed assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
+
+class Student < User
+    attr_reader :knowledge
+
+
+    def initialize
+        @knowledge = []
+
+    end
+
+    def learn(subs)
+        @knowledge << subs
+
+    end
 
 end
+
+levy = Student.new
+levy.first_name=("loop")
+p levy.first_name
